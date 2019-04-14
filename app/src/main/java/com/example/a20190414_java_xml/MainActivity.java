46785314a1a2465a0900307a617f12125ddc096f
jjysh1 <jjyshj@kcpmed.com>
2019-04-14 11:13:24 +0900

    id로 자바와 xml연결 변수에 담기

--- app/src/main/java/com/example/a20190414_java_xml/MainActivity.java
+++ app/src/main/java/com/example/a20190414_java_xml/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.a20190414_java_xml;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    TextView contentTextView;
+    EditText inputEditText;
+    Button okBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("메인 onCreate테스트","실행이 되는가?");
         Log.e("메인 e로그","되가?");
+
+        contentTextView = (TextView)findViewById(R.id.contentTextView);
+        inputEditText = (EditText)findViewById(R.id.inputEditText);
+        okBtn = (Button)findViewById(R.id.okBtn);
+
+
+
+
 
     }
 }
